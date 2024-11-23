5c044aaab9a2a810e1cd9e1a8f8196fed7d8281b
Isaiah Duarte <Isaiah.f.duarte@gmail.com>
2024-11-22 23:07:18 -0500

    Building

--- setup_script.ps1
+++ setup_script.ps1
@@ -1,3 +1,3 @@
-﻿# Download Console
-Invoke-WebRequest -Uri "https://downloads.controlup.com/console/9.0.5.426/ControlUp.zip" -OutFile "C:\setup_files\ControlUp.zip"
-Expand-Archive -Path "C:\setup_files\ControlUp.zip" -DestinationPath "C:\setup_files"
+﻿Write-Host "Installing ControlUp Automation Module"
+Install-Module -Name ControlUp.Automation -Verbose
+Write-Host "ControlUp Automation Module installed successfully"
