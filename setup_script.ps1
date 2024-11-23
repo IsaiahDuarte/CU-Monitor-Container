c0986efdb5d20f0bd11ceffdd6bf0c8d2fcd57d0
Isaiah Duarte <Isaiah.f.duarte@gmail.com>
2024-11-22 23:03:03 -0500

    Building

--- setup_script.ps1
+++ setup_script.ps1
@@ -1,1 +1,3 @@
-﻿Test-NetConnection -ComputerName google.com -Port 80
+﻿# Download Console
+Invoke-WebRequest -Uri "https://downloads.controlup.com/console/9.0.5.426/ControlUp.zip" -OutFile "C:\setup_files\ControlUp.zip"
+Expand-Archive -Path "C:\setup_files\ControlUp.zip" -DestinationPath "C:\setup_files"
