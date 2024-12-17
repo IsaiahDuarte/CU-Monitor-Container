166ce11cc3191c005fc9ac22ae836e9b89e6e894
Isaiah Duarte <Isaiah.f.duarte@gmail.com>
2024-12-17 07:23:43 -0500

    docker network create --driver transparent --subnet=172.20.0.0/16 cu_network

---
+++ agent/install/entrypoint.ps1
@@ -0,0 +1,25 @@
+﻿if(Get-Service *cuagent*) {
+    Write-Host "ControlUp monitor service detected. Starting service"
+    Get-Service *cuagent* | Start-Service
+
+    Write-Host "ControlUp monitor service started. Waiting for service to stop"
+    While((Get-Service "*cuagent*").Status -eq 'Running') { Start-Sleep -Seconds 3600 }
+} else {
+    Write-Host "Lodctr /R"
+    Lodctr /R
+
+    Write-Host 'Installing ControlUp Automation Module'
+    Install-PackageProvider -Name NuGet -Force -Confirm:$false
+    Install-Module -Name ControlUp.Automation -Force -SkipPublisherCheck -Scope AllUsers
+    Write-Host 'ControlUp Automation Module Installed'
+    
+    Write-Host 'Installing the CUAgent'
+    Import-Module ControlUp.Automation -Force
+    $splat = @{ 
+        Token = $env:Token
+        AgentAuthenticationKey = $env:AgentAuthKey
+        AgentRegistrationKey = $env:AgentRegKey
+        Site = $ENV:SiteName
+    }
+    Install-CUAgent @splat
+}
---
+++ monitor/install/entrypoint.ps1
@@ -0,0 +1,29 @@
+﻿if(Get-Service *Monitor*) {
+    Write-Host "ControlUp monitor service detected. Starting service"
+    Get-Service *Monitor* | Start-Service
+
+    Write-Host "ControlUp monitor service started. Waiting for service to stop"
+    While((Get-Service "*Monitor*").Status -eq 'Running') { Start-Sleep -Seconds 3600 }
+} else {
+    Write-Host "Lodctr /R"
+    Lodctr /R
+    
+    Write-Host 'Installing ControlUp Automation Module'
+    Install-PackageProvider -Name NuGet -Force -Confirm:$false | Out-Null
+    Install-Module -Name ControlUp.Automation -Force -SkipPublisherCheck -Scope AllUsers | Out-Null
+    Write-Host 'ControlUp Automation Module Installed'
+    
+    Import-Module ControlUp.Automation -Force
+
+    $splat = @{ 
+        Token = $env:Token
+        InternalDNSName = $ENV:COMPUTERNAME + '.' + $ENV:DomainName
+        SiteName = $ENV:SiteName
+    }
+
+    Write-Host "Installing ControlUp monitor service"
+    
+    Install-CUMonitor @splat
+    Write-Host "ControlUp monitor service started. Waiting for service to stop"
+    While((Get-Service "*Monitor*").Status -eq 'Running') { Start-Sleep -Seconds 3600 }
+}
